5732decdb2efa3e23296c80067d1707a39150262
raphiphat8888 <raphiphat.s@ku.th>
2026-09-12 17:10:04 +0700

    Fix: Ensure application runs by adding missing app.Run() call

--- todo-workshop/TodoApi/Program.cs
+++ todo-workshop/TodoApi/Program.cs
@@ -36,6 +36,8 @@ app.MapGet("/api/todos/{id}", (int id) =>
         : Results.Ok(todo);
 });
 
+app.Run();
+
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
